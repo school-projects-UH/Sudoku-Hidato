4bf3ec7d04eb1c6fd178af666789dd4721004c06
Tonito <otano0398@gmail.com>
2020-10-15 02:05:54 -0700

    Added type signature for the validPosition method

--- main.hs
+++ main.hs
@@ -1,12 +1,17 @@
 import System.IO
 import Printm
 
-validPosition board position = 
+validPosition :: [[Int]] -> (Int, Int) -> Bool
+validPosition board position =
     row >= 0 &&
     row < length board &&
     column >= 0 &&
     column < length (board !! row)
     where (row, column) = position
+
+explore board position targetPosition
+    | position == targetPosition = (True, board)
+
 
 main = do
     let matrix = [[1,2,3], [9,50,101]]
