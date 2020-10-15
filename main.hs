19ec913eba222db10c7c524374d589e8f6021869
Tonito <otano0398@gmail.com>
2020-10-15 15:50:46 -0700

    Functions for accessing and modifyng matrixes

--- main.hs
+++ main.hs
@@ -9,12 +9,20 @@ validPosition board position =
     column < length (board !! row)
     where (row, column) = position
 
-explore :: [[Int]] -> (Int,Int) -> (Int,Int) -> (Bool, [[Int]])
-explore board position targetPosition
-    | position == targetPosition = (True, board)
-    | not(validPosition board position) = (False, board)
+get board position = (board !! x !! y) where (x, y) = position
 
+replace [] _ _= []
+replace (_:xs) 0 a = a:xs
+replace (x:xs) n a =
+  if n < 0
+    then x:xs
+    else x: replace xs (n-1) a
+
+setm [] _ = []
+setm (r: matrix) (x, y, v)
+    | x > 0 = r: setm matrix (x-1, y, v)
+    | x == 0 = (replace r (y,v)):matrix
+    | otherwise = r:matrix
 
 main = do
-    let z = explore [[1, 2, 3]] (-1, 10) (10, 10)
-    print z
+    print (replace [2, 3, 4] 2 10)
