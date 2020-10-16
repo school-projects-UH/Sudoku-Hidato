ee7a7d744282bb9177f4b17498d0d9f28cdd40ae
Ginrod <gilbecubano@gmail.com>
2020-10-16 12:27:04 -0400

    solving hidatos

--- main.hs
+++ main.hs
@@ -1,6 +1,7 @@
 import System.IO
 import Printm
 import Fillm
+import Tests
 import Data.Foldable (toList)
 import Data.Sequence (fromList)
 
@@ -50,12 +51,38 @@ solve matrix start end empty
         validResults = filter (\(r, _) -> r == True) results
         v = (get matrix start) + 1
 
+runSamples samples = do
+    let runs = map (\(m, s, e, empty) -> solve m s e empty) samples
+    map (\run -> printRun run) runs
+
+printRun (result, board)
+    | result = do
+        print result
+        printm board
+    | otherwise =
+        print result
+
+uhSampleBoard =
+        [ [00, 33, 35, 00, 00, -1, -1, -1]
+        , [00, 00, 24, 22, 00, -1, -1, -1]
+        , [00, 00, 00, 21, 00, 00, -1, -1]
+        , [00, 26, 00, 13, 40, 11, 11, -1]
+        , [27, 00, 00, 00, 09, 00, 01, -1]
+        , [-1, -1, 00, 00, 18, 00, 00, -1]
+        , [-1, -1, -1, -1, 00, 07, 00, 00]
+        , [-1, -1, -1, -1, -1, -1, 05, 00]
+        ]
+
+uhSampleMinPos = (4, 6)
+uhSampleMaxPos = (3, 4)
+
 main = do
-    let matrix = [[3, 0, 1]]
-    let    start = (0, 2)
-    let    end = (0, 0)
-    let    empty = 0
+    -- runSamples [(uhSampleBoard, uhSampleMinPos, uhSampleMaxPos, 0)]
+    -- let (m, s, e, empty) = head tests
+    -- let (result, board) = solve m s e empty
+    -- print result
+    -- printm board
 
-    print (solve matrix start end empty)
-
-
+    let (result, board) = solve uhSampleBoard uhSampleMinPos uhSampleMaxPos 00
+    print result
+    printm board
